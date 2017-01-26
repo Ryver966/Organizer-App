627c4c26de2763641301d5f082e5cc351f5d33db
Ryver966 <p.gorski-96@wp.pl>
2017-01-26 19:13:36 +0100

    0.0.1

--- organizer.js
+++ organizer.js
@@ -1,14 +1,18 @@
 window.onload = function () {
     const main = document.getElementsByClassName('main')[0];
     console.log(main);
+    const list = document.getElementsByClassName('list')[0];
+    console.log(list);
+    const task = document.getElementsByClassName('taskName')[0];
+    console.log(task);
 
-    window.goToMain = function/* goToMain*/() {
+    window.goToMain = function () {
         console.log(main);
         document.getElementsByClassName('lockScreen')[0].style.display = 'none';
         main.style.visibility = 'visible';
     };
 
-    window.logIn = function /*logIn*/() {
+    window.logIn = function () {
         const user = document.getElementById('userName');
         const pass = document.getElementById('password');
         console.log(user.value);
@@ -20,52 +24,52 @@ window.onload = function () {
             alert('Wrong user name or passwod. Try again.');
         };
     };
-};
 
-function doSth(event, element) {
-    const x = event.keyCode;
-    if (x === 13) {
-        const loginBut = document.getElementsByClassName(element)[0];
-        loginBut.click();
+    window.doSth = function (event, element) {
+        const x = event.keyCode;
+        if (x === 13) {
+            const loginBut = document.getElementsByClassName(element)[0];
+            loginBut.click();
+        };
+    };
+
+    window.newTask = function (taskName) {
+        //Label
+        const newlabel = document.createElement("Label");
+        newlabel.for = taskName;
+        newlabel.innerHTML = taskName.value;
+        newlabel.className = 'taskLabel';
+        //Delete icon
+        const deleteIcon = document.createElement('i');
+        deleteIcon.className = 'material-icons';
+        deleteIcon.innerHTML = 'delete';
+        //Checkbox
+        const newCheckbox = document.createElement('input');
+        newCheckbox.type = 'checkbox';
+        newCheckbox.className = 'taskCheckbox';
+        //Adding all to element
+        list.appendChild(newCheckbox);
+        list.appendChild(newlabel);
+        list.appendChild(document.createElement('br'));
+        list.appendChild(document.createElement('hr'));
+
+        task.value = '';
+    };
+
+    window.addTask = function () {
+        const warning = document.getElementsByClassName('warning')[0];
+        if (task.value.length === 0) {
+            warning.style.display = 'block';
+        } else {
+            warning.style.display = 'none';
+            const addToList = newTask(task);
+        };
     };
 };
 
-const list = document.getElementsByClassName('list')[0];
-console.log(list);
-const task = document.getElementsByClassName('taskName')[0];
-console.log(task);
 
-function newTask(taskName) {
-//Label
-    const newlabel = document.createElement("Label");
-    newlabel.for = taskName;
-    newlabel.innerHTML = taskName.value;
-    newlabel.className = 'taskLabel';
-//Delete icon
-    const deleteIcon = document.createElement('i');
-    deleteIcon.className = 'material-icons';
-    deleteIcon.innerHTML = 'delete';
-//Checkbox
-    const newCheckbox = document.createElement('input');
-    newCheckbox.type = 'checkbox';
-    newCheckbox.className = 'taskCheckbox';
-//Adding all to element
-    document.getElementsByClassName('list')[0].appendChild(newCheckbox);
-    document.getElementsByClassName('list')[0].appendChild(newlabel);
-    document.getElementsByClassName('list')[0].appendChild(document.createElement('br'));
-    document.getElementsByClassName('list')[0].appendChild(document.createElement('hr'));
 
-    const task = document.getElementsByClassName('taskName')[0];
-    task.value = '';
-};
 
-function addTask() {
-    const warning = document.getElementsByClassName('warning')[0];
-    
-    if (task.value.length === 0) {
-        warning.style.display = 'block';
-    } else {
-        warning.style.display = 'none';
-        const addToList = newTask(task);
-    }
-}
+
+
+
